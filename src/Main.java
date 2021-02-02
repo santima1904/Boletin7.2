1223d5c5b4590daa22aae63824a9dd08af313d7c
SantiMartinez <santiago.martinez@iesnervion.es>
2021-02-02 20:49:11 +0100

    Ejercicio 1 pruebas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        /*
         Rectangulo rectangulo1 = new Rectangulo();
         Circulo circulo1 = new Circulo();
 
@@ -7,7 +8,11 @@ public class Main {
         rectangulo1.setBase(5);
 
         circulo1.setRadio(4);
-
+        */
+        Ejercicio1 intento = new Ejercicio1();
+        System.out.println(intento.getAbierto());
+        intento.abrirCerrojo(2);
+        System.out.println(intento.getAbierto());
 
     }
 }
